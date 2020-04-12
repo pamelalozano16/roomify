58bb32144c747e297b71feaf79fbf5b77b7d5bd8
pamelalozano16 <30474787+pamelalozano16@users.noreply.github.com>
2020-04-11 22:56:53 -0500

    Login despues del registro en get auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 //@desc Test route
-router.get("/", (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    //Regresa el user del id del token con toda la info menos el password
+
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (e) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
