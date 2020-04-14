601c1e8daad53ee48900f994ea845905be301de3
pamelalozano16 <30474787+pamelalozano16@users.noreply.github.com>
2020-04-13 20:56:32 -0500

    auth and loadUser

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,7 +2,7 @@ import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
-import { register } from "../../actions/auth";
+import { register, loadUser } from "../../actions/auth";
 import PropTypes from "prop-types";
 
 const Register = ({ setAlert, register }) => {
@@ -25,6 +25,7 @@ const Register = ({ setAlert, register }) => {
     } else {
       register({ name, email, password });
       console.log("SUCCESS");
+      loadUser();
     }
   };
 
