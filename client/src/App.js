ebf6718d591c9dcc903fcf7d1125c47a9b89c326
pamelalozano16 <30474787+pamelalozano16@users.noreply.github.com>
2020-04-22 17:02:10 -0500

    Create profile form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Fragment, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
-import { Landing } from "./components/layout/Landing";
+import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-form/CreateProfile";
 //Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -37,6 +38,11 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
